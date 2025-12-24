a35f546b0c64dc58ca3101c05b50f8408b2559a3
Blaire <113817141+hh-blaire-cho@users.noreply.github.com>
2025-12-24 20:33:39 +0900

    #1 기본 main.py lifespan 셋업 (시작할 때 데이터 주입)

---
+++ app/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO member (first_name, last_name, email) VALUES ('Peer', 'Simpson', 'adminuser@mail.com');
+INSERT INTO member (first_name, last_name, email) VALUES ('Jane', 'Doe', 'jane@mail.com');
